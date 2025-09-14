1abacd33c571709a22ec3b20e8e6b4aab6e8f96c
syednaveeth <syednaveeth709@gmail.com>
2025-09-14 16:25:38 +0530

    adding loging page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import Body from "./Components/Body";
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-center text-red-800 border h-20 p-20 text-2xl font-bold">
-        Hello Syed
-      </p>
+    <div>
+      <Body />
     </div>
   );
 }
---
+++ src/Components/Browser.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Browser = () => {
+  return <div>Browser loded</div>;
+};
+
+export default Browser;
